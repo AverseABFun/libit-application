1f21b1f1cc32169765739cf464e86fc8131d7716
Arthur Beck <averse.abfun@gmail.com>
2024-02-10 19:53:14 +0000

    fixed test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 
 import requests
 
-id = requests.put("localhost:5000", params={"note": "Test note"})["result"]
-assert requests.get("localhost:5000", params={"id": id})["result"]=="Test note"
-assert requests.get("localhost:5000", params={"id": "wsufusfhkdsjkjhfk"})["error"]
-assert not requests.post("localhost:5000", params={"id": id, "note": "Another test note"})["error"]
-assert requests.get("localhost:5000", params={"id": id})["result"]=="Another test note"
-assert requests.delete("localhost:5000", params={"id": id})["result"]=="Another test note"
-assert requests.delete("localhost:5000", params={"id": "kjzsdjksdf"})["error"]
-assert requests.get("localhost:5000", params={"id": id})["error"]
+id = requests.put("http://localhost:5000", params={"note": "Test note"}).json()["result"]
+assert requests.get("http://localhost:5000", params={"id": id}).json()["result"]=="Test note"
+assert requests.get("http://localhost:5000", params={"id": "wsufusfhkdsjkjhfk"}).json()["error"]
+assert not requests.post("http://localhost:5000", params={"id": id, "note": "Another test note"}).json()["error"]
+assert requests.get("http://localhost:5000", params={"id": id}).json()["result"]=="Another test note"
+assert requests.delete("http://localhost:5000", params={"id": id}).json()["result"]=="Another test note"
+assert requests.delete("http://localhost:5000", params={"id": "kjzsdjksdf"}).json()["error"]
+assert requests.get("http://localhost:5000", params={"id": id}).json()["error"]
